dce66885fb39d91f9dd5615eaf1e7ca6d5b51a76
zhangsan <zhangsan@itcast>
2020-11-28 22:31:16 +0800

    把工程添加到git

---
+++ gittestproject/src/main/java/com/itheima/first/firstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.first;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
