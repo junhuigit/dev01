dfe63152a5c9943c1f20e3b1ff11bfc432f71c8c
zhangsan <zhangsan@itcast>
2020-11-29 15:58:32 +0800

    第二次修改测试

--- SecondClass/src/main/java/com/itheima/test001.java
+++ SecondClass/src/main/java/com/itheima/test001.java
@@ -5,5 +5,6 @@ public class test001 {
         System.out.println("test001");
         System.out.println("test002");
         System.out.println("test003");
+        System.out.println("clone");
     }
 }
