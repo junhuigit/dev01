10373a67db67d7b00edfa618dd74149eaff2767c
zhangsan <zhangsan@itcast>
2020-11-29 15:02:01 +0800

    secondclass2

--- SecondClass/src/main/java/com/itheima/test001.java
+++ SecondClass/src/main/java/com/itheima/test001.java
@@ -1,5 +1,9 @@
 package com.itheima;
 
 public class test001 {
-
+    public static void main(String[] args) {
+        System.out.println("test001");
+        System.out.println("test002");
+        System.out.println("test003");
+    }
 }
